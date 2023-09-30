ebce7e52f2fd0d5c4baf1951ef5338f9a238fd59
khrisna-yudhap <86khrisnayudha@gmail.com>
2023-09-30 20:06:56 +0700

    responsive halaman tagihan

--- assets/page.js
+++ assets/page.js
@@ -8,3 +8,27 @@ function showContent() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("content").style.display = "block";
 }
+
+// select table row
+
+$(document).ready(function () {
+  $("#bill-table")
+    .find("tbody")
+    .on("click", "tr", function () {
+      if ($(this).find(":checkbox").is(":checked")) {
+        $(this).find(":checkbox").prop("checked", false);
+      } else {
+        $(this).find(":checkbox").prop("checked", true);
+      }
+    });
+
+  $("#bill-table")
+    .find("tbody")
+    .on("click", ":checkbox", function () {
+      if ($(this).is(":checked")) {
+        $(this).prop("checked", false);
+      } else if ($(this).not(":checked")) {
+        $(this).prop("checked", true);
+      }
+    });
+});
